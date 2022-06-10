81d1594566f5e91f3e6f450e664132122792c966
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-06-10 13:24:17 +0530

    s1=input().lower().split() s2=input().lower().split() c=0 if len(s1)>len(s2):     for i in s2:         if i in s1 and s1.count(i)==1 and s2.count(i)==1:             c+=1     print(c)     exit() for i in s1:     if i in s2 and s2.count(i)==1 and s1.count(i)==1:         c+=1
    print(c)

---
+++ common_words_-1.py
@@ -0,0 +1,16 @@
+s1=input().lower().split()
+s2=input().lower().split()
+c=0
+if len(s1)>len(s2):
+    for i in s2:
+        if i in s1 and s1.count(i)==1 and s2.count(i)==1:
+            c+=1
+    print(c)
+    exit()
+for i in s1:
+    if i in s2 and s2.count(i)==1 and s1.count(i)==1:
+        c+=1
+    
+print(c)            
+            
+    
