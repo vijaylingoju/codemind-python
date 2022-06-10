5f588256215826f22cab92894f6f87684291025b
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-06-10 11:01:07 +0530

    s=list(input().lower()) while ' ' in s:     s.remove(' ') print(''.join(sorted(set(s))))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,9 +1,4 @@
 s=list(input().lower())
-p='abcdefghijklmnopqrstuvwxyz'
-ns=''
 while ' ' in s:
     s.remove(' ')
-for i in p:
-    if s.count(i)==1:
-        ns=ns+i
-print(ns)        
+print(''.join(sorted(set(s))))
