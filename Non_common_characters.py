09996879254e60b43c94d5f1e1d5fbb913d73f95
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-06-10 11:34:28 +0530

    s1=list(input().lower()) s2=list(input().lower()) p='abcdefghijklmnopqrstuvwxyz' s=0 for i in p:     if i in s1 and i not in s2 or i not in s1 and i in s2:         s+=1 print(s)

---
+++ Non_common_characters.py
@@ -0,0 +1,8 @@
+s1=list(input().lower())
+s2=list(input().lower())
+p='abcdefghijklmnopqrstuvwxyz'
+s=0
+for i in p:
+    if i in s1 and i not in s2 or i not in s1 and i in s2:
+        s+=1
+print(s)        
