cecc1e2677be740e0c6701ff12066bda9a11081d
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-06-21 14:50:43 +0530

    from math import sqrt def isprime(n):     if n==1:         return 0     for i in range(2,int(sqrt(n))+1):         if n%i==0:             return 0     return 1
    n=int(input())
    c=0
    for i in range(1,n+1):
        if n%i==0 and isprime(i)==0:
            c+=1
    print(c)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,3 +1,4 @@
+
 from math import sqrt
 def isprime(n):
     if n==1:
@@ -5,11 +6,11 @@ def isprime(n):
     for i in range(2,int(sqrt(n))+1):
         if n%i==0:
             return 0
-    return 1        
+    return 1
 
 n=int(input())
 c=0
 for i in range(1,n+1):
-    if isprime(i)==0 and n%i==0:
+    if n%i==0 and isprime(i)==0:
         c+=1
-print(c)        
+print(c)            
