c1dc7b9119a729bebf67700d85682347cbfe629e
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-07-02 11:15:28 +0530

    a='abcdefghijklmnopqrstuvwxyz' s=list(input().lower()) while ' ' in s:     s.remove(' ') x=[] for i in a:     if (i in s and s.count(i)==1) and i not in x:         x.append(i) print(''.join(x))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,4 +1,9 @@
+a='abcdefghijklmnopqrstuvwxyz'
 s=list(input().lower())
 while ' ' in s:
     s.remove(' ')
-print(''.join(sorted(set(s))))
+x=[]    
+for i in a:
+    if (i in s and s.count(i)==1) and i not in x:
+        x.append(i)
+print(''.join(x))        
