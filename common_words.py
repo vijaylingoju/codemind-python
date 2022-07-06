c567c05c38851d36d7cd32ddf842980c27ce99f6
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-07-06 23:12:57 +0530

    s1=input().lower().split() s2=input().lower().split() s=s2+s1 x=[] for i in s:     if s.count(i)==2 and i not in x:         x.append(i) print(*x)

--- common_words.py
+++ common_words.py
@@ -1,7 +1,8 @@
 s1=input().lower().split()
 s2=input().lower().split()
-f,x=-1,[]
-for i in s2:
-    for j in s1:
-        if i==j:
-            print(i,end=" ")
+s=s2+s1
+x=[]
+for i in s:
+    if s.count(i)==2 and i not in x:
+        x.append(i)
+print(*x)        
