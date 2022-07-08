6d128d67dfb0e3fef33f12c4fa490b191b75d9a5
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-07-09 00:54:05 +0530

    from math import sqrt def fun(n):     if n==1:         return 0     for i in range(2,int(sqrt(n))+1):         if n%i==0:             return 0     return 1 r1=int(input()) r2=int(input()) for i in range(r1,r2+1):     if fun(i)==1:         print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,14 +1,13 @@
 from math import sqrt
-def prime(n):
+def fun(n):
     if n==1:
         return 0
     for i in range(2,int(sqrt(n))+1):
         if n%i==0:
             return 0
-    return 1    
-
-a=int(input())
-b=int(input())
-for i in range(a,b):
-    if prime(i)==1:
+    return 1        
+r1=int(input())
+r2=int(input())
+for i in range(r1,r2+1):
+    if fun(i)==1:
         print(i)
