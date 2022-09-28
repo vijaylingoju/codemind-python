cab6793420aaf85cd8ff918960442e55b519048f
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-09-28 10:30:11 +0530

    s=input().lower() p="abcdefghijklmnopqrstuvwxyz" k='' for i in p:     if i in s and i not in k:         k+=i print(k)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,9 +1,9 @@
-a='abcdefghijklmnopqrstuvwxyz'
-s=list(input().lower())
-while ' ' in s:
-    s.remove(' ')
-x=[]    
-for i in a:
-    if (i in s and s.count(i)==1) and i not in x:
-        x.append(i)
-print(''.join(x))        
+s=input().lower()
+p="abcdefghijklmnopqrstuvwxyz"
+k=''
+for i in p:
+    if i in s and i not in k:
+        k+=i
+print(k)        
+        
+    
