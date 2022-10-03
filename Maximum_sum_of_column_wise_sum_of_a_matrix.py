cebd97a403f7358c70398794c8613fee0d575591
Vijay Lingoju <97424329+vijaylingoju@users.noreply.github.com>
2022-10-03 12:17:03 +0530

    r,c=map(int,input().split()) mat,mx,s=[],0,0 for i in range(r):mat.append(list(map(int,input().split()))) for i in range(c):     s=0     for j in range(r):         s+=mat[j][i]     mx=max(mx,s) print(mx)

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.py
@@ -0,0 +1,9 @@
+r,c=map(int,input().split())
+mat,mx,s=[],0,0
+for i in range(r):mat.append(list(map(int,input().split())))
+for i in range(c):
+    s=0
+    for j in range(r):
+        s+=mat[j][i]
+    mx=max(mx,s)    
+print(mx)    
